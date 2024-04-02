5d9e3905df9eaa7c957830aff1a5e4d09ef88d4e
Nicholas Hansen <nicholas.hansen101@gmail.com>
2024-04-02 16:45:44 +0200

    Add skeleton of DelilahContext

---
+++ src/runtime/local/context/DelilahContext.cpp
@@ -0,0 +1,29 @@
+#include "runtime/local/context/DelilahContext.h"
+#include <spdlog/spdlog.h>
+#include <stdio.h>
+
+
+
+using namespace std;
+
+void DelilahContext::destroy() spdlog::debug("Destroying FPGA context...");
+
+void DelilahContext::init()
+{
+    spdlog::debug("creating Delilah Context...");
+
+    /* Open Delilah device */
+    fd = open(path, O_RDWR);
+    if (fd < 0) {
+        perror("Cannot open Delilah device");
+        return 3;
+    }
+    //TODO: Add rest of init
+}
+std::unique_ptr<IContext> DelilahContext::createDelilahContext(int device_id) 
+{
+    auto ctx = std::unique_ptr<DelilahContext>(new DelilahContext(dev_id));
+    ctx->init();
+
+    return ctx;
+}
---
+++ src/runtime/local/context/DelilahContext.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cassert>
+#include <iostream>
+#include <memory>
+
+class DelilahContext : public IContext {
+    int device_id = -1;
+    size_t mem_budget = 0;
+
+    explicit DelilahContext(int id) : device_id(id) {
+	 
+	}
+public:
+    io_uring = NULL;
+    path = "/dev/delilah0";
+    fd = -1;
+
+    static std::unique_ptr<IContext> createDelilahContext(int id);
+
+private:
+    void init();
+}
